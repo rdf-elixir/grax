84b52cc9467bad789a60c30bd31c1644df9e79c3
Marcel Otto <marcelotto@gmx.de>
2020-12-21 12:27:44 +0100

    Add .iex.exs file

---
+++ .iex.exs
@@ -0,0 +1,35 @@
+import RDF.Sigils
+import RDF.Guards
+
+alias RDF.NS
+alias RDF.NS.{RDFS, OWL, SKOS}
+
+alias RDF.{
+  Term,
+  IRI,
+  BlankNode,
+  Literal,
+  XSD,
+
+  Triple,
+  Quad,
+  Statement,
+
+  Description,
+  Graph,
+  Dataset,
+
+  PrefixMap,
+  PropertyMap
+}
+
+alias RDF.BlankNode, as: BNode
+
+alias RDF.{NTriples, NQuads, Turtle}
+
+alias Decimal, as: D
+
+c "test/support/example_ns.ex"
+c "test/support/example_mappings.ex"
+
+alias Example.NS.EX
