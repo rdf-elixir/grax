5dba50cf4ad531ca0b09841d3346b3745b2d4ab9
Marcel Otto <marcelotto@gmx.de>
2022-06-29 10:03:05 +0200

    Ignore dialyzer warning

--- lib/grax/rdf/access.ex
+++ lib/grax/rdf/access.ex
@@ -9,6 +9,10 @@ defmodule Grax.RDF.Access do
   alias Grax.Schema.LinkProperty
   alias Grax.InvalidResourceTypeError
 
+  # TODO: this function becomes unnecessary when we depend on RDF.ex >= 0.12 as that's the default behaviour of Graph.description now
+  # Dialyzer raises a warning with RDF.ex 0.12, since the fallback will never be used, but we need
+  # the warning as long we support older versions, so we ignore the warning until we drop their support
+  @dialyzer {:nowarn_function, description: 2}
   def description(graph, id) do
     Graph.description(graph, id) || Description.new(id)
   end
