75dc64de223b1a2836a3a006d848f6bc122fc906
Marcel Otto <marcelotto@gmx.de>
2023-06-12 12:03:52 +0200

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -74,10 +74,10 @@ defmodule Grax.MixProject do
       rdf_ex_dep(:rdf, "~> 1.0"),
       {:yuri_template, "~> 1.1"},
       {:elixir_uuid, "~> 1.2"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.2", only: :dev, runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: :dev, runtime: false},
       {:ex_doc, "~> 0.29", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.15", only: :test},
+      {:excoveralls, "~> 0.16", only: :test},
       {:benchee, "~> 1.0", only: :dev}
     ]
   end
